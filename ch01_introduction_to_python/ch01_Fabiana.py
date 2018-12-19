02c634d6b23f49a73b3645680421513c8ed38cd3
Fabiana Lassandro <fabianalassandro@gmail.com>
2018-12-19 10:39:04 +0000

    Chapter 1 - with comments

---
+++ ch01_introduction_to_python/ch01_Fabiana.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#####################################
+
+#CHAPTER 01 - INTRODUCTION TO PYTHON
+
+####################################
+
+#*** How does the console work? ***
+# The console is the place where I can check the results of what I'm doing in this file by using the editor. 
+#PS: in Spyder the console is the panel on my bottom right corner. 
+
+
+#----------------------------------
+# Arithmetic Operators
+#----------------------------------
+
+A = 2 + 3
+B = 2.9 + 3.2
+C = (3.2 / 2.0) + 7
+D = 2**3
+E = 4**(1/2)
+F = str(6) + 'a'
+print (A)#-->5 | By using print() function I can see the result on the console
+print (B)#--> 6.1
+
+
+#----------------------------------
+# Print a string
+#----------------------------------
+print()
+print('Hello World')#-->Hello World | Here I'm printing a string. A string is a sequence of charachters.
+
