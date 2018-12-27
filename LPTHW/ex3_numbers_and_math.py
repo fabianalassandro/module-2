db6e25988fa808dd49c45e59120ef22a301aa37f
Fabiana Lassandro <fabianalassandro@gmail.com>
2018-12-27 10:51:00 +0100

    ex3 updated with comments

--- LPTHW/ex3_numbers_and_math.py
+++ LPTHW/ex3_numbers_and_math.py
@@ -10,19 +10,22 @@
 
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25 + 30 / 6)#--> 'Hens', 30
+#Before the division, then + 25
 
-print("Now I will count the eggs:")
+print("Roosters", 100 - 25 * 3 % 4)#--> 'Roosters', 97
+#Before the moltiplication (25*3=75), then the modulo operation (75%4=3), finally 100-3= 97
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("Now I will count the eggs:")#-->Now I will count the eggs:
 
-print("It is true that 3 + 2 < 5 - 7?")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)#--> 7
 
-print(3 + 2 < 5 - 7)
+print("It is true that 3 + 2 < 5 - 7?")#-->It is true that 3 + 2 < 5 - 7?
 
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 - 7)
+print(3 + 2 < 5 - 7)#--> False
+#By using <, >, <=, => the result is True or False
+print("What is 3 + 2?", 3 + 2)#-->'What is 3 + 2?', 5
+print("What is 5 - 7?", 5 - 7)#-->'What is 5 - 7?', -2
 
 print("Oh, that's why it's False.")
 
