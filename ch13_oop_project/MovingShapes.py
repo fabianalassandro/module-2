6f2c86ad9ada24a9a0714923b9c8e5396aab658a
Fabiana Lassandro <fabianalassandro@gmail.com>
2019-01-07 15:41:02 +0000

    chapter 13 - shapes exercise

--- ch13_oop_project/MovingShapes.py
+++ ch13_oop_project/MovingShapes.py
@@ -10,47 +10,36 @@ from pylab import random as r
 
 class MovingShape:
     def __init__(self,frame,shape,diameter):
-        self.x = 0
-        self.y = 0
-        
-        self.dx = 5 + 10 * r()
-        self.dy = 5 + 10 * r()
-        
         self.shape = shape
         self.diameter = diameter
         self.figure = Shape(shape,diameter)
         
-        self.goto(self.x, self.y)
+        self.minx = (frame.width*0.01) + self.diameter / 2
+        self.miny = (frame.height*0.01) + self.diameter / 2
         
-        self.minx = self.diameter
-        self.miny = self.diameter
-        
-        self.maxx = frame.width - self.diameter
-        self.maxy = frame.height - self.diameter
+        self.maxx = (frame.width*0.985) - self.diameter / 2
+        self.maxy = (frame.height*0.985) - self.diameter / 2
         
         self.x = self.minx + (r()* (self.maxx - self.minx))
         self.y= self.miny + (r()* (self.maxy - self.miny))
         
+        self.dx = 5 + 10 * r()
+        self.dy = 5 + 10 * r()
+         
+        self.goto(self.x, self.y)
+        
+ 
+        
     def goto(self,x,y):
         self.figure.goto(x,y)
   
-    def moveTick(self):
-        self.x += self.dx
-        self.y += self.dy
-        
-        if self.x >= self.maxx:
+    def moveTick(self):  
+        if self.x >= self.maxx or self.x <=self.minx:
             self.dx = self.dx * -1 
     
-        if self.y >= self.maxy:
+        if self.y >= self.maxy or self.y <=self.miny:
             self.dy=self.dy *-1
-            
-        if self.x <= self.minx:
-            self.dx = self.dx * -1 
-    
-        if self.y <= self.miny:
-            self.dy=self.dy *-1
-            
-
+           
         self.x= self.x +self.dx
         self.y= self.y+ self.dy 
         
@@ -59,17 +48,19 @@ class MovingShape:
 class Square(MovingShape):
     def __init__(self,frame,diameter):
         MovingShape.__init__(self,frame,'square',diameter)
-        self.minx = self.diameter/2
-        self.miny = self.diameter/2
         
-
 class Diamond(MovingShape):
     def __init__(self,frame,diameter):
         MovingShape.__init__(self,frame,'diamond',diameter)
-        self.diameter =2 *self.diameter
-        self.minx = self.diameter/2
-        self.miny = self.diameter/2
+#        self.diameter = (2*(diameter**2)**0.5) # this is to calculate the diameter of a diamond that is longer than a square diameter.
+        self.minx = (frame.width*0.009) + self.diameter / 2
+        self.miny = (frame.height*0.009) + self.diameter / 2
+        
+        self.maxx = (frame.width*0.98) - self.diameter / 2
+        self.maxy = (frame.height*0.965) - self.diameter / 2
         
 class Circle(MovingShape):
     def __init__(self,frame,diameter):
         MovingShape.__init__(self,frame,'circle',diameter)
+        
+# Change colour and size by moving - take a look at the code fo beginners book      
