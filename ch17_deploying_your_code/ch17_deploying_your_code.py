a8e7b98f3e75d2e2db29e3ce307198f9367fc58e
Fabiana Lassandro <fabianalassandro@gmail.com>
2019-01-22 14:56:06 +0000

    chapter 17

---
+++ ch17_deploying_your_code/ch17_deploying_your_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 13:38:44 2019
+
+@author: Fabiana
+"""
+
+#####################################
+
+# CHAPTER 17 - DEPLOYING YOUR CODE
+
+#####################################
+
+#  Go to this link to check the application online: https://stark-shore-59149.herokuapp.com/
